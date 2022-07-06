986d5b7852dd4d35a1c85df9b963b9face92096b
qwet2342 <hojikabirhikmatov77@gmail.com>
2022-07-07 03:52:41 +0500

    'start'

---
+++ src/components/Madaniyat/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css';
+
+export default () => {
+  return <div>
+    
+    Rustama  aka sizniki shet footer bilan
+    madaniyat lifstayl sport ham sizga hammasi bir xil faqat datasi boshqa 
+
+  </div>;
+};
---
+++ src/components/Mahalliy/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+  return <div>
+    
+    Muhammadrasul  footer bilan
+    mahalliy pul dunyo ham senga hammasi bir xil faqat datasi boshqa holos
+
+  </div>;
+};
